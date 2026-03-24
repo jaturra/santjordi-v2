9b712293f6ba1cf13caa5aac94fbe8f6cda88190
Jose Angel <turrado.joseangel@gmail.com>
2026-03-24 19:40:39 +0100

    Actualización de configuración para Coolify

---
+++ frontend/src/pages/Bebidas/Bebidas.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Bebidas() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h1>🍷 Carta de Begudes</h1>
+      <p>Pròximament...</p>
+    </div>
+  );
+}
